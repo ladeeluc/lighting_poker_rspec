2d37316272d25cedef5047da3fb82242a1a92643
Ladeeluc <laura.hatch.lh@gmail.com>
2020-03-16 14:06:38 -0500

    updaates

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,9 @@
 class Card
+    def self.build(suit, rank)
+        new(suit: suit, rank: rank)
+    end 
+    private_class_method :new 
+
     def initialize(suit:, rank:)
             @suit = suit
             @rank = case rank
@@ -12,9 +17,19 @@ class Card
 def suit
      @suit
 end
- 
+  
 def rank
     @rank
 end 
-end
 
+def ==(other)
+    rank == other.rank && suit == other.suit
+    end
+    
+    def hash
+        [rank, suit].hash
+    end
+
+    def eql?(other)
+        self == other
+end    
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,39 +1,91 @@
-require 'spec_helper'
-
+require 'set'
 require 'card'
 
-RSpec.describe 'a playing card' do
-    it 'has a suit' do
-        raise unless Card.new(suit: :spades, rank: 4 ).suit == :spades 
+ describe Card do
+    def card(params = {})
+        defaults = {
+            suit:  :hearts,
+            rank: 7,
+        }
+
+#RSpec.describe 'a playing card' do
+
+Card.bulid(*defaults.merge(params).values_at(:suit, :rank))
     end
-    it 'has a rank' do
-        raise unless Card.new(suit: :spades, rank: 4).rank ==  4
-    end
+
+it 'has a suit' do
+    raise unless card(suit: :spades).suit == :spades 
+end
+
+it 'has a rank' do
+    raise unless card(rank: 4).rank ==  4
+end
+
+it 'is equal to itself' do
+    subject = card(suit: :spades, rank: 4)
+    other = card(suit: :spades, rank: 4)
+
+    raise unless subject == other
+end  
+
+
+it 'is hash equal to itself'do
+subject = card(suit: :spades, rank: 4)  
+other = card(suit: :spades, rank: 4)
+
+raise unless Set.new([subject, other]).size == 1
+end
+
+it 'is not hash equal to itself'do
+    subject = card(suit: :spades, rank: 4)  
+    other = card(suit: :spades, rank: 4)
+
+    raise unless Set.new([subject, other]).size == 2
+end
+
+it 'is not hash equal to a card of differing suit' do
+    subject = card(suit: :spades, rank: 4)
+    other = card(suit: :hearts, rank: 4)
+    
+    raise unless Set.new([subject, other]).size == 2
+end
+
+it 'is not equal to a card of differing rank' do
+    subject = card(suit: :spades, rank: 4)
+    other = card(suit: :spades, rank: 5)
+
+    raise unless subject != other
+end
+it 'is not hash equal to a card of differing rank' do
+    subject = card(suit: :spades, rank: 4)
+    other = card(suit: :spades, rank: 5)
+
+    raise unless Set.new([subject, other]).size == 2
+end
+
 
     describe 'a jack' do 
         it 'ranks higher than a 10' do
-        lower = Card.new(suit: :spades, rank:  10)
-        higher = Card.new(suit:  :spades, rank: :jack)
-    
+        lower = card(rank:  10)
+        higher = card(rank: :jack)
         raise unless higher.rank > lower.rank
       end
     end
     
     describe 'a queen' do
         it 'ranks higher than a jack' do
-        lower = Card.new(suit: :spades, rank:  :jack)
-        higher = Card.new(suit:  :spades, rank: :queen)
-
+        lower = card(rank:  :jack)
+        higher = card(rank: :queen)
         raise unless higher.rank > lower.rank
     end
 end
 
 describe 'a king' do
     it 'ranks higher than a queen' do
-        lower = Card.new(suit: :spades, rank:  :queen)
-        higher = Card.new(suit:  :spades, rank: :king)
-
+        lower = card(rank:  :queen)
+        higher = card(rank: :king)
         raise unless higher.rank > lower.rank
     end
  end
 end
+ 
